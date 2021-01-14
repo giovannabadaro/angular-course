49139602b9bfc62179f6413770124cbd48bc8dee
giovanna <giovannabadaroalves@gmail.com>
2021-01-13 22:31:56 -0300

    data biding

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
-import { CourseService } from './../../../../../course-manager-master/course-manager-master/src/app/courses/course.service';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -8,11 +8,25 @@ import { CourseService } from './../../../../../course-manager-master/course-man
 })
 
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy: string;
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
-   this.courses = this.courseService.retrieveAll();
+   this._courses = this.courseService.retrieveAll();
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter() {
+    return this._filterBy;
   }
 }
