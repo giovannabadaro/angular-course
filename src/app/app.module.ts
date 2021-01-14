0820b9d744941e6864218754bdcdb4f2d07e47c3
giovanna <giovannabadaroalves@gmail.com>
2021-01-14 01:22:05 -0300

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404 } from './error-404/error-404.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',
+        component: Error404
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
